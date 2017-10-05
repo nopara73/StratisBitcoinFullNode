0480e97a25fb7c9430de0dcd12718f5d7ca639be
nopara73 <adam.ficsor73@gmail.com>
2017-10-06 00:19:55 +0100

    Fix merge conflicts

--- Stratis.Bitcoin.Features.Wallet/KeyManagement/KeyManager.cs
+++ Stratis.Bitcoin.Features.Wallet/KeyManagement/KeyManager.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Security;
+using Stratis.Bitcoin.Utilities.Extensions;
 
 namespace Stratis.Bitcoin.Features.Wallet.KeyManagement
 {
